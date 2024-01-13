af4b050bb40c6c3d99d5833adaaa6e7c48f0aef8
Nitin Ahuja <53057442+ahnitin@users.noreply.github.com>
2024-01-13 23:17:31 +0530

    Integrating the frontend with backend

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const User = require("./models/user");
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:127.0.0.1:5500",
+    methods:["GET","POST","PUT","DELETE"]
+}));
 app.use(express.static("public"));
 app.use(userRoutes);
 app.use((req,res)=>{
